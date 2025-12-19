12fe1cc7924a66534c0d193129cd2ffeab5a9cf4
Henry Odhiambo <jokababa52@gmail.com>
2025-12-19 14:22:36 +0300

    Update inquiry.js

--- js/inquiry.js
+++ js/inquiry.js
@@ -1,20 +1,18 @@
-<script>
+document.addEventListener("DOMContentLoaded", () => {
+
+  /* COUNTRY CALLING CODES */
   const countryCallingCodes = {
-    "Kenya": "+254",
-    "Tanzania": "+255",
-    "Uganda": "+256",
-    "Rwanda": "+250",
+    Kenya: "+254",
+    Tanzania: "+255",
+    Uganda: "+256",
+    Rwanda: "+250",
     "South Africa": "+27",
     "United States": "+1",
     "United Kingdom": "+44",
-    "Australia": "+61",
-    "Canada": "+1",
-    "India": "+91",
-    "United Arab Emirates": "+971",
-    "France": "+33",
-    "Germany": "+49",
-    "Italy": "+39",
-    "Ireland": "+353"
+    Canada: "+1",
+    Australia: "+61",
+    India: "+91",
+    "United Arab Emirates": "+971"
   };
 
   const countrySelect = document.getElementById("country");
@@ -23,14 +21,8 @@
   if (countrySelect && phoneInput) {
     countrySelect.addEventListener("change", () => {
       const code = countryCallingCodes[countrySelect.value];
-      if (code) {
-        phoneInput.value = code + " ";
-        phoneInput.focus();
-      }
+      if (code) phoneInput.value = code + " ";
     });
   }
-</script>
 
-<script src="/js/nav.js" defer></script>
-<script src="/js/inquiry.js" defer></script>
-
+});
